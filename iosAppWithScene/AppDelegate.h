8306b4f433b6bb6fffc185c417923445fc4b6c52
Brad Barrows <bebrws@gmail.com>
2020-04-20 13:49:01 -0700

    Initial Commit

---
+++ iosAppWithScene/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iosAppWithScene
+//
+//  Created by Brad Barrows on 4/20/20.
+//  Copyright © 2020 bbarrows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iosAppWithScene/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosAppWithScene
+//
+//  Created by Brad Barrows on 4/20/20.
+//  Copyright © 2020 bbarrows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
